cf83af80289d89609cc3c101444ee4db9598aa07
truongminhluong <truongminhluong2003@gmail.com>
2023-11-05 15:41:36 +0700

    a

--- BaiTapTuLuan/main.swift
+++ BaiTapTuLuan/main.swift
@@ -7,5 +7,41 @@
 
 import Foundation
 
-print("Hello, World!")
+//Bài 1
+//let keys = [1, 2]
+//let values = ["Lượng", "Phương"]
+//let myDictionary = Dictionary(uniqueKeysWithValues: zip(keys, values))
+//print(myDictionary)
+
+//Bài 2
+//if let input = readLine(), let num = Int(input) {
+//    
+//    var count = 0
+//    for i in 1 ... num {
+//        if i % 2 != 0 {
+//            count += 1
+//        }
+//    }
+//    if count % 2 == 0 {
+//        print("Tổng các số nguyên từ 1 đến \(num) là số chẵn")
+//    }else {
+//        print("Tổng các số nguyên từ 1 đến \(num) là số lẻ")
+//    }
+//}
+
+//Bài 3
+//let arr1:[Int] = [1, 2, 3, 4]
+//let arr2:[Float] = [0.1, 0.2, 0.3, 0.4]
+//var newArr:[Float] = []
+//
+//for i in 0 ..< arr1.count{
+//    var num = Float(arr1[i]) + arr2[i]
+//    newArr.append(num)
+//}
+//newArr.sort()
+//for value in newArr {
+//    print(value)
+//}
+
+
 
